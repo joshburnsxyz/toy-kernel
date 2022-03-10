a8018a9c9a51a2b0d4cc8e412f4918b2ee7a2384
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-03-10 14:36:05 +1100

    Interrupt Descriptor Table header

---
+++ idt.h
@@ -0,0 +1,11 @@
+struct IDT_entry {
+  unsigned short int offset_lowerbits;
+  unsigned short int selector;
+  unsigned char zero;
+  unsigned char type_attr;
+  unsigned short int offset_higherbits;
+}
+
+struct IDT_entry IDT[IDT_SIZE];
+
+void idt_init(void);
